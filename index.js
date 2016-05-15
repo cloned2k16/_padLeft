8bac0cc28023bd795e8aaba436cc1d22384d9315
Paolo <paolo@yaoo.net>
2016-05-15 22:22:37 +0200

    0.0.3

--- index.js
+++ index.js
@@ -1,33 +1,95 @@
-{
-        "name"              :   "pad_left"
-    ,   "version"           :   "0.0.3"
-    ,   "description"       :   "String left pad"
-    ,   "license"           :   "CC-BY-NC-4.0"
-    ,   "readmeFilename"    :   "README.md"
-    ,   "keywords"          :   [
-                                    "padleft"
-                            ,       "leftpad"
-                            ,       "left"
-                            ,       "pad"
-                            ,       "padding"
-                            ,       "string"
-                            ,       "repeat"
-                            ]
-    ,   "bugs"              :   {
-                                    "url"   :   "https://github.com/cloned2k16/_padLeft/issues"
-                            }
-    ,   "dependencies"      :   {}
-    ,   "devDependencies"   :   {
-                                    "tape"  :   "~4.5.1"
-                                }
-    ,   "repository"        :   {
-                                    "type"  :   "git"
-                                ,   "url"   :   "git@github.com:cloned2k16/_padLeft.git"
-                                
-                            }
-    ,   "main"              :   "index.js"
-    ,   "scripts"           :   {
-                                    "test"  :   "node test"
-                            }
-    ,   "author"            :   "Paolo Lioy"
+'use strict';
+
+var _padLeft        =   { 
+                        name:       'Padding from Left side (aka _padLeft)' 
+                    ,   desc:       'A module handling Left alignment for string'
+                    ,   version:    '0.0.3'
+                    };
+
+_padLeft.spaces     = [ // avoid global pollution & power of 2 size
+    ''
+  , ' '                 //1
+  , '  '                //2
+  , '   '           
+  , '    '              //4
+  , '     '         
+  , '      '
+  , '       '
+  , '        '          //8
+  , '         '
+  , '          '
+  , '           '
+  , '            '
+  , '             '
+  , '              '
+  , '               '
+  , '                '  //16
+];
+
+_padLeft.zeros      = [ // that's another most used case, worth to cache it
+    ''
+  , '0'                 //1
+  , '00'                //2
+  , '000'           
+  , '0000'              //4
+  , '00000'         
+  , '000000'
+  , '0000000'
+  , '00000000'          //8
+  , '000000000'
+  , '0000000000'
+  , '00000000000'
+  , '000000000000'
+  , '0000000000000'
+  , '00000000000000'
+  , '000000000000000'
+  , '0000000000000000'  //16
+];
+
+_padLeft.func       = function  (str, len, ch)  {
+  var   pad     = ''
+  ,     cache   
+  ,     chSz
+  ,     strSz
+  ;
+  
+  str       = '' + str; 
+  ch        = arguments.length<=2 ? ' ' : ''+ch;
+  chSz      = ch .length;
+  strSz     = str.length;
+
+  // we now treat chSz > 1 as new feature which let you pad a string with another string
+  // while in this case the len argument is interpreted as ''repeat'' padding string n times
+  len       = chSz <=0 ?        0 
+                : chSz ==1 ?    len - strSz
+                    :           len 
+            ; 
+            
+  if (len   <= 0) return str;
+  
+  // special cases
+        if (ch === ' ') cache = _padLeft.spaces;
+  else  if (ch === '0') cache = _padLeft.zeros;
+  
+  if (cache){  
+   var cLen=cache.length-1;
+   if (len <= cLen) return cache[len] + str;
+   len-=cLen;
+   pad =cache[cLen]
+  }
+  
+  do {
+   pad += (len & 1) ?  ch : '';
+   len >>>=1;
+   ch+=ch;
+  }  
+  while (len > 0);
+
+  return pad + str;
 }
+
+
+var module          = module || {};          // browser friendly ... :D
+module.exports      = _padLeft.func;
+
+
